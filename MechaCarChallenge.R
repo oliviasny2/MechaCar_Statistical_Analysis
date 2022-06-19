268cdb6d72af7b7438be1945696f0d91f06cd011
Olivia Snyder <oliviasny2@gmail.com>
2022-06-19 19:18:13 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,14 @@ summary(Mecha_Car_lm)
 # Deliverable 2 - Summary Stats
 total_summary <- Suspension_Data %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
 lot_summary <- Suspension_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+# Deliverable 3 - t-test
+t.test(Suspension_Data$PSI,mu=1500)
+
+lot1 <- subset(Suspension_Data, Manufacturing_Lot == 'Lot1')
+lot2 <- subset(Suspension_Data, Manufacturing_Lot == 'Lot2')
+lot3 <- subset(Suspension_Data, Manufacturing_Lot == 'Lot3')
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
